113357fc78d7ec118e38dcaa18329005b209c44f
lualuna13 <163040028+lualuna13@users.noreply.github.com>
2025-02-12 13:53:18 -0600

    Actividad final

--- 11-ejercicio-lista-tareas/script.js
+++ 11-ejercicio-lista-tareas/script.js
@@ -7,32 +7,54 @@ const taskList = document.getElementById('taskList');
 // Escuchador
 
 addButton.addEventListener('click', creatTask);
+
+//El usuario da Enter y se crea la tarea
+//addButton.addEventListener('keydown', creatTask);
+
+
 // Función manejadora externa
 function creatTask () {
    if (taskInput.value) {
-      //crear elemento que intregra una tarea
-      console.log(taskItem.classList);
-
+   
       //Con esto se crea el contenedor de una tarea
       const taskItem = document.createElement('div')
       taskItem.classList.add('task');
+
       //Creamos parrafo de la tarea
       const taskText = document.createElement('p');
       taskText.innerText = taskInput.value;
+
       //Creamos el contenedor de los iconos
       const taskIcons = document.createElement('div');
-      taskIcons.classList.add('task-icon');
+      taskIcons.classList.add('task-icons');
+
       //Crear el icono de check
       const iconCheck = document.createElement('i');
-      iconCheck.classList.add('bi', 'bi-chack-circle', 'icon-complete');
+      iconCheck.classList.add('bi', 'bi-check-circle', 'icono-complete');
+
       //Crear el icocno de trash
       const iconDelete = document.createElement('i');
       iconDelete.classList.add('bi', 'bi-trash', 'icon-delete')
+
        //Estructura de los elemrntos
        taskIcons.append(iconCheck, iconDelete);
-       taskItem.append(taskText. taskIcons);
+       taskItem.append(taskText,taskIcons);
        taskList.append(taskItem);
+
+       //Escuchadores de los iconos
+       iconCheck.addEventListener('click', (e) => {
+         e.target.parentNode.parentNode.classList.toggle('complete');
+       });
+
+       //Definir un escuchador al elemento iconDelete
+       iconDelete.addEventListener('click', (e) => {
+       e.target.parentNode.parentNode.remove();
+       });
       
+   
+     
+
+    //Regresamos el valor vacio de Input  
     taskInput.value = '';
    }else {
     alert('Hey escribiste una tarea');
